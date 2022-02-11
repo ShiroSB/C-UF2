6bba007e8982b6daef1e79de4528f1a4fee5352c
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-02-11 14:24:16 +0100

    Create functions.py

---
+++ Python/Act_8/functions.py
@@ -0,0 +1,15 @@
+def inp_num():
+    
+    x = int(input("Introduce un numero : "))
+    
+    return x
+   
+def change_value(x,i):
+    
+    aux = x 
+    
+    x = i 
+    
+    i = aux
+    
+    return x,i
