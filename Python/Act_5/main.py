ca8bfb138af0633fafb87b7b784e9894bcac1ccd
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-02-11 14:25:51 +0100

    Create main.py

---
+++ Python/Act_5/main.py
@@ -0,0 +1,14 @@
+import functions
+
+def main():
+    
+    x = functions.inp_num()
+    
+    i = functions.inp_num()
+    
+    x,i = functions.change_value(x,i)
+    
+    print(x,i)
+    
+if __name__ == '__main__':
+    main()
