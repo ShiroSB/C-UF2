918fdda17562e91e7476c0c9ac8ad3ec15feb29d
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-02-11 14:40:39 +0100

    Create main.py

---
+++ Python/Act_8/main.py
@@ -0,0 +1,14 @@
+import functions
+
+def main():
+    
+    x = functions.inp_num()
+    
+    i = functions.inp_num()
+    
+    res = functions.elev(x,i)
+    
+    print(res)
+    
+if __name__ == '__main__':
+    main()
