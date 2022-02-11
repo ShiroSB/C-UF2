de9fb00f03cec3d2df29d05632a2b96cdd552950
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-02-11 15:06:29 +0100

    Create main.py

---
+++ Python/Act_6/main.py
@@ -0,0 +1,13 @@
+import functions
+
+def main():
+    
+    x = functions.inp_num()
+
+    res = functions.secuencia(x)
+    
+    print ("Resultado suma : ",res)
+ 
+    
+if __name__ == '__main__':
+    main()
