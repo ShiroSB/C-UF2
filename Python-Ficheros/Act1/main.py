2b3586b8978689a14e75df0a503c8bce65134f09
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-02-25 14:55:05 +0100

    Create main.py

---
+++ Python-Ficheros/Act1/main.py
@@ -0,0 +1,16 @@
+#Implementa un programa que demani un text a l’usuari i el guardi
+#en un fitxer anomenat “text.txt”.
+
+import functions
+
+def main():
+    
+    ruta = functions.def_ruta()
+    
+    functions.write_file(ruta)
+    
+    functions.read_file(ruta)
+    
+if __name__ == '__main__':
+    
+    main()
