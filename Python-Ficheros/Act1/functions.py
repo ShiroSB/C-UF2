b2d870fa3a81cf21f501c93aa0ec42c26faebc1f
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-02-25 14:55:39 +0100

    Create functions.py

---
+++ Python-Ficheros/Act1/functions.py
@@ -0,0 +1,27 @@
+TEXTO1 = 'Introduce el texto:  '
+
+TEXTO2 = 'text.txt'
+
+def read_file(fname):
+    
+    file = open(fname,'r')
+    
+    print(file.read())
+    
+    file.close()
+    
+def write_file(fname):
+    
+    frase = input(TEXTO1)
+    
+    file = open(fname,'w')
+    
+    file.write(frase)
+    
+    file.close()
+    
+def def_ruta():
+    
+    x = TEXTO2
+    
+    return x
