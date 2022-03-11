f74045df1790db31734233b7886cc0306bc8ad6d
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-03-11 15:14:17 +0100

    Create functions.py

---
+++ Python-Ficheros/Act9/functions.py
@@ -0,0 +1,45 @@
+import csv
+import  pandas as pd
+import frases as fr
+
+def pedir_datos(x):
+
+    write_header()
+    for i in range(x):
+        identificador = int(input(fr.FRASE1))
+        cantante = input(fr.FRASE2)
+        cancion = input(fr.FRASE3)
+        fecha = input(fr.FRASE4)
+        visualizaciones = int(input(fr.FRASE5))
+
+        musica = {
+            "identificador":[identificador],
+            "cantante":[cantante],
+            "cancion":[cancion],
+            "fecha":[fecha],
+            "visualizaciones":[visualizaciones]
+        }
+        write_CSV(musica)
+        print_panda(musica)
+
+def print_panda(musica):
+    panda = pd.DataFrame(musica)
+    print(panda)
+
+def read_CSV():
+    with open('music.csv') as csvfile:
+        read_csv = csv.reader(csvfile, delimiter=',')
+        for row in read_csv:
+            print(row)
+
+def write_CSV(std):
+    with open('music.csv', 'a', encoding='utf-8', newline='') as csvfile:
+        fieldnames = ['identificador', 'cantante', 'cancion', 'fecha', 'visualizaciones']
+        write_csv = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        write_csv.writerow(std)
+
+def write_header():
+    with open('music.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        fieldnames = ['identificador', 'cantante', 'cancions', 'fecha', 'visualizaciones']
+        write_csv = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        write_csv.writeheader()
