794503dcef47be4da9b7670231a8b1b257c305fa
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-05-20 00:52:05 +0200

    Update main.py

--- Python-Ficheros/SumCol/main.py
+++ Python-Ficheros/SumCol/main.py
@@ -1,29 +1,9 @@
 import csv
-
+import functions as f
 def main():
 
-    with open('consumo_energia.csv') as projects_board:
-        readCSV = csv.reader(projects_board,delimiter = ',')
-        count = 0
-        x = 0
-        C_3_TOTAL = 0
-        y = 0
-        consumo = 0
-        for row in readCSV:
-            count = count + 1
-            if count >= 2:
-                if row[7] == '':
-                    row[7] = '0'
-                x = int(row[5])
-                y = int(row[7])
-
-                if x == 3:
-                    C_3_TOTAL = C_3_TOTAL + 1
-                    consumo = int(consumo) + y
-        print(C_3_TOTAL)
-        print(consumo)
-
-
+    f.consumo_total()
+    f.print_datos()
 
 if __name__ == "__main__":
     main()
